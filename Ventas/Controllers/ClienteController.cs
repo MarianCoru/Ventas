f9523dc0f16398825c69dad9859d59671be9dce0
MarianCoru <mcmaryxp@gmail.com>
2017-12-29 22:53:24 -0400

    listo

---
+++ Ventas/Controllers/ClienteController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ventas.Models;
+
+namespace Ventas.Controllers
+{
+    public class ClienteController : Controller
+    {
+        VENTASEntities cnx; 
+        public ClienteController()
+        {
+            cnx = new VENTASEntities();
+        }
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+        public ActionResult Guardar(int rut, string nombre, string apellido, string direccion, string tipocliente)
+        {
+
+            Ventas.Models.CLIENTE cliente = new Ventas.Models.CLIENTE
+            {
+
+                Rut = rut,
+                Nombre = nombre,
+                Apellido = apellido,
+                Direccion = direccion,
+                Tipo = tipocliente
+            };
+
+            cnx.CLIENTE.Add(cliente);
+            cnx.SaveChanges();
+
+
+            return View("Listado", ListadoClientes());
+        }
+        public ActionResult Ficha(int rut)
+        {
+            return View(cnx.CLIENTE.Where(x => x.Rut == rut).First());
+        }
+        public ActionResult Listado()
+        {
+            return View("Listado", ListadoClientes());
+        }
+        private List<Ventas.Models.CLIENTE> ListadoClientes()
+        {
+            cnx.Database.Connection.Open();
+            List<Ventas.Models.CLIENTE> cli = cnx.CLIENTE.ToList();
+            cnx.Database.Connection.Close();
+            return cli;
+        }
+        public ActionResult Eliminar(int rut)
+        {
+            cnx.CLIENTE.Remove(cnx.CLIENTE.Where(x => x.Rut.Equals(rut)).First());
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.CLIENTE.ToList());
+
+        }
+        
+    }
+}
+
---
+++ Ventas/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ventas.Models;
+
+namespace Ventas.Controllers
+{
+    public class HomeController : Controller
+    {
+        VENTASEntities cnx;
+        public HomeController()
+        {
+            cnx = new VENTASEntities();
+        }
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        public ActionResult Listado()
+        {
+            return View("Listado", ListadoProductos());
+        }
+        private List<Ventas.Models.PRODUCTO> ListadoProductos()
+        {
+            cnx.Database.Connection.Open();
+            List<Ventas.Models.PRODUCTO> produc = cnx.PRODUCTO.ToList();
+
+            cnx.Database.Connection.Close();
+
+            return produc;
+        }
+        public ActionResult Guardar(int codigobarra, string nombre, string familia, int precio, int descuentotope, string descripcion)
+        {
+            PRODUCTO pro = new PRODUCTO()
+            {
+                CodigoBarra = codigobarra,
+                Nombre = nombre,
+                Familia = familia,
+                Precio = precio,
+                DescuentoTope = descuentotope,
+                Descripcion = descripcion
+            };
+
+            cnx.PRODUCTO.Add(pro);
+            cnx.SaveChanges();
+            return View("Listado", ListadoProductos());
+        }
+        public ActionResult Ficha(int codigobarra)
+        {
+
+            return View(BuscarProducto(codigobarra));
+        }
+        public PRODUCTO BuscarProducto(int codigobarra)
+        {
+            PRODUCTO nueva = new PRODUCTO();
+            foreach (PRODUCTO pro in cnx.PRODUCTO.ToList())
+            {
+                if (pro.CodigoBarra.Equals(codigobarra))
+                {
+                    nueva = pro;
+                }
+            }
+            return nueva;
+        }
+        public ActionResult Ver(int codigobarra)
+        {
+            return View("Ficha", null);
+        }
+    }
+}
